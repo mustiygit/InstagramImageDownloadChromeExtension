ae61ab4a2f2495200f454e52012c34734566aa65
Savaş Can ALTUN <savascanaltun@gmail.com>
2016-07-15 04:16:52 +0300

    Version 3.0

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# InstagramImageDownloadChromeExtension
-İnstagram image download chrome extension open source
+İnstagram İmage Download Chrome Extension
+=========
+
+İnstagram resim indirme chrome eklentisi kaynak dosyalarıdır. 
+
+
+Chrome Sayfası => [Tıklayınız](http://goo.gl/MTmv1e)
+
+
+
+Eklenecekler 
+=========
+- Çoklu dil dessteği eklenti bazlı yapılacak
+- Multi resim sayfalarında ki sorun giderilecek.
+
+
+Author : Savaş Can Altun
+Mail : savascanaltun@gmail.com
+Web : [http://savascanaltun.com.tr](http://savascanaltun.com.tr)
---
+++ instagramresimindirme.js
@@ -0,0 +1,38 @@
+/*
+* Developer : Savaş Can ALTUN <savascanaltun@gmail.com>
+* Github : http://github.com/saltun
+* Pages : http://instagramresimindirme.net
+* Todo : multi lang extension
+*/
+$(function(){
+
+var userLang = navigator.language || navigator.userLanguage; 
+
+/* Run 3 seconds */
+setInterval(function(){
+
+/* get page url */
+var page=document.URL;
+
+/* Check page */
+if (page.indexOf('/p/') > 0 ) {
+
+	/* button text */
+	if(userLang=="tr"){
+		var downloadText="İndir";
+	}else{
+		var downloadText="Download";
+	}
+	
+	/* download link */
+	var link="http://instagramresimindirme.net/down.php?url="+page;
+
+	$('._ovg3g').html('<a href="'+link+'" target="_blank" style=" display: inline-block; height: 38px; padding: 0 30px; color: #555; text-align: center; font-size: 11px; font-weight: 600; line-height: 38px; letter-spacing: .1rem; text-transform: uppercase; text-decoration: none; white-space: nowrap; background-color: transparent; border-radius: 4px; border: 1px solid #bbb; cursor: pointer; box-sizing: border-box; color: #FFF; background-color: #ff0000; border-color: #ff0000;">'+downloadText+' </a>');
+
+}
+
+}, 3000);
+
+
+
+});
