29134b5b47d5766d80d77335c6206326a090b4cf
Savaş Can ALTUN <savascanaltun@gmail.com>
2016-07-17 04:01:16 +0300

    Versiyon 3.2 and New Language

--- instagramresimindirme.js
+++ instagramresimindirme.js
@@ -2,7 +2,6 @@
 * Developer : Savaş Can ALTUN <savascanaltun@gmail.com>
 * Github : http://github.com/saltun
 * Pages : http://instagramresimindirme.net
-* Todo : multi lang extension
 */
 $(function(){
 
@@ -17,17 +16,11 @@ var page=document.URL;
 /* Check page */
 if (page.indexOf('/p/') > 0 ) {
 
-	/* button text */
-	if(chrome.i18n.getMessage("appLang")=="tr"){
-		var downloadText="İndir";
-	}else{
-		var downloadText="Download";
-	}
 	
 	/* download link */
 	var link="http://instagramresimindirme.net/down.php?url="+page;
 
-	$('._ovg3g').html('<a href="'+link+'" target="_blank" style=" display: inline-block; height: 38px; padding: 0 30px; color: #555; text-align: center; font-size: 11px; font-weight: 600; line-height: 38px; letter-spacing: .1rem; text-transform: uppercase; text-decoration: none; white-space: nowrap; background-color: transparent; border-radius: 4px; border: 1px solid #bbb; cursor: pointer; box-sizing: border-box; color: #FFF; background-color: #ff0000; border-color: #ff0000;">'+downloadText+' </a>');
+	$('._ovg3g').html('<a href="'+link+'"><img src="http://uptc.xyz/fcghj.png" style="margin-left:10px;margin-top:5px;" title="Save"></a>');
 
 }else{
 	$('._ovg3g').html('');
